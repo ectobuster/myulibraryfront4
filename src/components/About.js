f4b4289e659a11da172ca77b91800450651b2594
Roberto Villalta <roberto15394@gmail.com>
2024-05-15 19:10:02 -0600

    frontend setup using vite

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>About our library...</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/interfaces.ts
@@ -0,0 +1,11 @@
+// interfaces.ts
+
+export interface Books {
+    id: number;
+    title: string;
+    author: string;
+    published_year: number;
+    genre: string;
+    available: boolean;
+  }
+  
